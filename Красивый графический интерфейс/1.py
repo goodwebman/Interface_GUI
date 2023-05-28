7c2772d7c90658f95a802cfb5b89c89f10176620
goodwebman <124707601+goodwebman@users.noreply.github.com>
2023-05-28 23:07:44 +0600

    Add files via upload

---
+++ Красивый графический интерфейс/1.py
@@ -0,0 +1,23 @@
+import dearpygui.dearpygui as dpg
+
+# Установка: pip install dearpygui
+
+def save_callback():
+    print("Save Clicked")
+
+dpg.create_context()
+dpg.create_viewport() # 3 обязательные команды
+dpg.setup_dearpygui()
+
+
+
+
+with dpg.window(label="Example Window"):
+    dpg.add_text("Hello world")
+    dpg.add_button(label="Save", callback=save_callback)
+    dpg.add_input_text(label="string")
+    dpg.add_slider_float(label="float")
+
+dpg.show_viewport()
+dpg.start_dearpygui() # 3 обязательные команды
+dpg.destroy_context()
